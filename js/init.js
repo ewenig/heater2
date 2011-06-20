22ee68c59083516f7c4bb6d97c87c479a3b58a1d
ewenig <esw1726@rit.edu>
2011-06-20 14:18:52 -0400

    fixes

--- js/init.js
+++ js/init.js
@@ -1,6 +1,6 @@
-var sideLength = $(document).width / 60;
+var sideLength = $(document).width() / 60;
 var canv = document.getElementById("heater2");
 $(function() {
-	canv.width = $(document).width;
+	canv.width = $(document).width();
 	canv.height = sideLength * 6.5;
 });
